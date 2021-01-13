ab204c6941c3efbab06fdf51a16f9e7a68f75d61
Wrumz <bsaachirs@gmail.com>
2021-01-13 14:33:15 +0700

    First Commit

---
+++ Javascript Crash Course/home.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+alert("Hello Let's Learn JS");
+
+//Variables
+var b = 'smoothie';
+var someNumber = 45;
+
+console.log(b);
+console.log(someNumber);
+
+document.getElementById()
