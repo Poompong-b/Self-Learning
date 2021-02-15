f4d51dc5bc60859a858be4d0b1908b9f58f779e0
Wrumz <bsaachirs@gmail.com>
2021-02-15 15:49:43 +0700

    REACT Full Course

---
+++ React Crash Course/REACT Full Course/react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const UseStateBasics = () => {
+  const [text, setText] = useState("random title");
+
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("Hello World");
+    } else {
+      setText("random title");
+    }
+  };
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Chagne Title
+      </button>
+    </>
+  );
+};
+
+export default UseStateBasics;
